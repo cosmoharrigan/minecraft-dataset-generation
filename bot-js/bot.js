1d97713a018220de0e5ea972d38191fee1077d1a
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2015-06-29 05:20:22 -0700

    add javascript bot

---
+++ bot-js/bot.js
@@ -0,0 +1,102 @@
+// Bot for collecting training data
+
+// Requires https://github.com/andrewrk/mineflayer
+// Usage: node bot.js
+
+// Set the following parameters before running:
+CONFIG_EMAIL = ''
+CONFIG_PASSWORD = ''
+CONFIG_SERVER = 'localhost'
+CONFIG_PORT = 25565
+
+var fs = require('fs');
+var os = require('os');
+var path = require('path');
+
+// create and connect the bot
+var mineflayer = require('mineflayer');
+var bot = mineflayer.createBot({
+  host: CONFIG_SERVER,
+  port: CONFIG_PORT,
+  username: CONFIG_EMAIL,
+  password: CONFIG_PASSWORD, });
+
+bot.on('login', function() {
+  main();
+});
+
+// returns an integer between min (included) and max (excluded)
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function choose(choices) {
+  var index = Math.floor(Math.random() * choices.length);
+  return choices[index];
+}
+
+function main() {
+  i = 0;
+  block = {};
+  block.x = 0;
+  block.y = 0;
+  block.z = 0;
+
+  // initialize the environment so that it starts empty
+  for (x = -16; x < 16; x++) {
+    for (y = 2; y < 16; y++) {
+    	for (z = -16; z < 16; z++) {
+  			bot.chat('/setblock ' + x + ' ' + y + ' ' + z + ' air');
+  		}
+  	}
+  }
+
+  // This function will run every 500 milliseconds
+  setInterval(function(){
+    // set a new position every 100 iterations
+    if (i % 10 == 0) {
+      x = getRandomInt(-10, 11);
+      z = getRandomInt(-14, -3);
+      bot.chat('/tp ' + x + ' ' + 2 + ' ' + z);
+    }
+
+    // update the pitch and yaw every iteration
+    // yaw = (i / 100) * 6.28;
+    yaw = Math.random()*0.60 - 0.30 + 3.14;
+    pitch = Math.random()*0.40 - 0.20;
+    bot.look(yaw, pitch);
+
+    // update the block every 100 iterations
+  	if (i % 100 == 0) {
+  	  // remove the previous block
+  		bot.chat('/setblock ' + block.x + ' ' + block.y + ' ' + block.z + ' air');
+  		block.x = getRandomInt(-10, 11);
+  		block.y = getRandomInt(2, 11);
+  		block.z = getRandomInt(2, 15);
+
+      // choose the new block type
+      blockTypes = ['stone', 'wood', 'leaves', 'ice', 'pumpkin']
+      blockType = choose(blockTypes)
+
+      // set the new block
+  		bot.chat('/setblock ' + block.x + ' ' + block.y + ' ' + block.z + ' ' + blockType);
+
+      // make sure the lighting stays the same
+      bot.chat('/time set 0');
+  	}
+
+    // log the current parameters
+    console.log(Date.now().toString() + ',' +  // timestamp
+                block.x + ',' +                // block x
+                block.y + ',' +                // block y
+                block.z + ',' +                // block z
+                blockType + ',' +              // block type
+                bot.entity.position.x + ',' +  // bot x
+                bot.entity.position.y + ',' +  // bot y
+                bot.entity.position.z + ',' +  // bot z
+                bot.entity.pitch + ',' +       // bot pitch
+                bot.entity.yaw);               // bot yaw
+
+  	i++;
+  }, 1 * 500);  // 500 is the delay between updates in milliseconds
+}
