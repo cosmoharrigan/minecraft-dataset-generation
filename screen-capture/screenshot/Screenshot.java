5bfef52be67f63b5ff1e9c64521e0b45b98e6ccf
Cosmo Harrigan <cosmo.harrigan@singularityu.org>
2015-06-29 15:52:23 -0700

    update screenshot; trigger at 1Hz

--- screen-capture/screenshot/Screenshot.java
+++ screen-capture/screenshot/Screenshot.java
@@ -7,22 +7,33 @@ import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 import javax.imageio.ImageIO;
 
-// Takes a series of screenshots and saves them as PNG files with an incrementing integer as the filename
+// Takes a series of screenshots and saves them as PNG files 
+// with the timestamp in milliseconds as the filename
 public class Screenshot {
-    public static void main(String[] args) throws AWTException, IOException, InterruptedException {
-        int i = 0;
+    public static void main(String[] args) throws AWTException, IOException, InterruptedException {  
+        final Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+        final Robot robot = new Robot();
+        
+        Runnable task = new Runnable() {
+            public void run() {
+                BufferedImage capture = robot.createScreenCapture(screenRect);
+                long timestamp = System.currentTimeMillis();
+                try {
+                    ImageIO.write(capture, "png", new File(timestamp + ".png"));
+                } catch (IOException e){
+                    System.out.println("Exception: " + e);
+                }
+                System.out.println(timestamp);
+            }
+        };
 
-        Rectangle screenRect = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-        Robot robot = new Robot();
-
-        while(true) {
-            BufferedImage capture = robot.createScreenCapture(screenRect);
-            long timestamp = System.currentTimeMillis();
-            ImageIO.write(capture, "png", new File(Integer.toString(i) + "_" + timestamp + ".png"));
-            i++;
-            System.out.println(i + ": " + timestamp);
-        }
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
     }
 }
